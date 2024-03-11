7f0ff0dc95a7a72ce887e15597cc8bbe765f52fa
Anton <antonsoluha@gmail.com>
2024-03-11 13:39:32 +0200

    Fixed bugs and style footer

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,37 @@ function burgerMenu() {
             body.classList.remove('locked')
         }
     })
+
+const menuLinks = document.querySelectorAll('.menu__item-link[data-goto]');
+if (menuLinks.length > 0) {
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+});
+    
+    
+    function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
+
+        if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        burger.classList.remove('active');
+        body.classList.remove('locked');
+        }
+
+
+        window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+        });
+        e.preventDefault();
+    }
+    }
+}
+
+
     // Вот тут мы ставим брейкпоинт навбара
     window.addEventListener('resize', () => {
         if (window.innerWidth > 991.98) {
